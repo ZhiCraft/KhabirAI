7bea107ddcd6bc73205746c462c7ddd7f1871d02
Jianxingxu63 <Jianxingxu63@gmail.com>
2025-06-30 15:08:59 +0800

    init

---
+++ src/test/java/com/zhicraft/admin/rag/AzureOpenAIEmbeddingTest.java
@@ -0,0 +1,28 @@
+package com.zhicraft.admin.rag;
+
+import org.junit.runner.RunWith;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * TDDO
+ *
+ * @author Xu JianXing
+ * @since 0.1
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class AzureOpenAIEmbeddingTest {
+    @Autowired
+    private EmbeddingModel embeddingModel;
+
+    public void test() {
+
+
+        
+
+
+    }
+}
